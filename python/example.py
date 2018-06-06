7fbc24d6c1353caa067fb8aee14ffc54ec61e709
Chris Adams <cadams@loc.gov>
2018-06-06 14:30:55 -0400

    Update Python examples
    * Use Black formatting
    * Apply isort config for first-party imports
    * Suggest format for long implicit string concatenations

--- python/example.py
+++ python/example.py
@@ -1,23 +1,32 @@
 # encoding: utf-8
+# This is the sort order enforced by isort: future, stdlib, third-party, first-party:
 from __future__ import absolute_import, division, print_function
 
-# This is the sort order enforced by isort: stdlib, third-party, first-party:
 from functools import partial
 
-from example_utils import hello_world
 from requests import session
+
+from example_utils import hello_world
 
 unused_session = partial(session)
 
 print(hello_world())
-print({'a single key': 'and value can be one line'})
-print({'but all keys': 'and values',
-       'should be indented': 'at the same level'})
 
-print({
-    'this key is really long and the value is even worse':
-        'so it should not be on a single line',
-    'plus there is more than one value':
-        'and we do not want to pack more than one assignment onto a'
-        ' single line'
-})
+print({"a single key": "and value can be one line"})
+
+print(
+    {
+        "but all keys": "and values which do not fit on one line",
+        "should be indented": "at the same level",
+    }
+)
+
+print(
+    {
+        "this key is even longer and": (
+            "the value is a long literal which should be on multiple"
+            " lines for clarity with parentheses to clearly denote that"
+            " each of the implicit concatenations are at the same level"
+        )
+    }
+)
